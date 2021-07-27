92b04acb1b10bcdb9e9870a3406f79fbbf783413
Nathan Sinnathurai <nathansinnathurai@Nathans-MacBook-Pro.local>
2021-07-27 00:47:46 -0400

    adding back end incomplete back end logic

--- tenthousand-photos/src/app/photos/photo.interface.ts
+++ tenthousand-photos/src/app/photos/photo.interface.ts
@@ -1,4 +1,5 @@
 export interface Photo {
   caption: string;
   imagePath: string;
+  id: string;
 }
--- tenthousand-photos/src/app/photos/photo.service.ts
+++ tenthousand-photos/src/app/photos/photo.service.ts
@@ -1,30 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Photo } from './photo.interface';
 
+@Injectable()
 export class PhotoService {
   private photos: Photo[] = [
-    {
-      caption: 'Ezra is hungry!',
-      imagePath: 'assets/aadhav-bites-foot.jpeg',
-    },
-    {
-      caption: 'I learned how to open the window!',
-      imagePath: 'assets/aadhav-window.jpeg',
-    },
   ];
 
   photoUpdate: Subject<Photo[]> = new Subject<Photo[]>();
+
+  constructor(private http: HttpClient) {}
 
   uploadPhoto(caption: string, imagePath: string) {
     const newPhoto: Photo = {
       caption,
       imagePath,
+      id: '',
     };
-    this.photos.push(newPhoto);
-    this.photoUpdate.next(this.photos.slice());
+    this.http.post<{photos: Photo[]}>('http://localhost:3000/api/photos', newPhoto).subscribe((res) => {
+      console.log(res);
+      this.photos.push(newPhoto);
+      this.photoUpdate.next(this.photos.slice());
+    })
   }
 
   getPhotos() {
-    return this.photos.slice();
+    this.http.get<{photos: Photo[]}>('http://localhost:3000/api/photos').subscribe((res) => {
+      this.photos = res.photos;
+      this.photoUpdate.next(this.photos.slice());
+    });
   }
 }
